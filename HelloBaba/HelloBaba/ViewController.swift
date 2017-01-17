2cdd410a2d1e5241f5b804c497c37fb189e932b2
Maya Lekova <apokalyptra@gmail.com>
2017-01-17 10:34:51 +0100

    Add some actions to the first view

--- HelloBaba/HelloBaba/ViewController.swift
+++ HelloBaba/HelloBaba/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printBox: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func printToBox(_ sender: Any) {
+        printBox.text = "Hello world"
+    }
 
 }
 
